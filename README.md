e2a213cea017a449e897b1cb67102b203261a587
Zineb112 <57940406+Zineb112@users.noreply.github.com>
2020-03-17 22:00:00 +0100

    WorkShop1

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # JavaScript-learn
+
+Zineb Belassiri
+
+Zinebbelassiri123@gmail.com
---
+++ WorkShop1.js
@@ -0,0 +1,12 @@
+var str1 = "Myname";
+var str2 ="Myname";
+
+function Comp(a,b){
+    if (str1 == str2){
+        console.log(true);
+    }
+    else {
+        console.log(false);
+    }
+}
+Comp(str1,str2);
